afbea5be0fe69ee969f1091ee2c3148d9139e61c
NE0ID <neov0id.nova@gmail.com>
2023-09-04 02:50:13 -0700

    [Add] 0-minoperations.py (tested)

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Hbtn - Minimum operations: Return minimum operations to reach n """
+
+
+def minOperations(n):
+    """ """
+    copyAll = 0
+    paste = 0
+    character = 1
+    stock = 0
+
+    if (n == 0):
+        return (0)
+
+    while (character < n):
+        if ((character * 2) <= n):
+            copyAll += 1
+            stock = character
+        paste += 1
+        character += stock
+        if (character > n):
+            return (0)
+    return (copyAll + paste)
