a76d4ac283b3ced298b9ad9692ec49863331550b
NE0ID <neov0id.nova@gmail.com>
2023-09-04 03:17:29 -0700

    [Add] insert_in_sorted_linked_list given files + 0-insert_number.c file

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert a node in a sorted linked list
+ *
+ * @head: head of list to work on
+ * @number: value of node to insert
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node;
+	listint_t *tmp;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+
+	tmp = malloc(sizeof(listint_t));
+	if (tmp == NULL)
+		return (NULL);
+
+	node->n = number;
+
+	if (head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		if (tmp->next->n > number)
+		{
+			node->next = tmp->next;
+			tmp->next = node;
+			return (node);
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
