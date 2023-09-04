53168dae2c5c5151a6042e1d1c1a477bf5a65aa4
NE0ID <neov0id.nova@gmail.com>
2023-09-04 13:17:56 -0700

    [Try] append to unlocked without getting out of range exception

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,13 +6,11 @@ def canUnlockAll(boxes):
     unlocked = [boxes[0]]
     keys = []
 
-    """for box in boxes:"""
-
     for box in unlocked:
         for key in box:
             if key not in keys:
-                if key <= len(boxes):
-                    keys.append(key)
+                keys.append(key)
+
         for key in keys:
             if boxes[key] not in unlocked:
                 unlocked.append(boxes[key])
