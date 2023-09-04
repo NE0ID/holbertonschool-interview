d7323addfd2f43ee7e9b3ee2427024a3d3ae4b95
NE0ID <neov0id.nova@gmail.com>
2023-09-04 05:05:12 -0700

    [Try] main case - fixing

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,16 +6,16 @@ def canUnlockAll(boxes):
     unlocked = [boxes[0]]
     keys = []
 
-    for box in boxes:
-        if box in unlocked:
-            for key in box:
-                if key not in keys:
-                    keys.append(key)
+    """for box in boxes:"""
+
+    for box in unlocked:
+        for key in box:
+            if key not in keys:
+                keys.append(key)
+
         for key in keys:
-            if boxes[key] in boxes:
-                if boxes[key] not in unlocked:
-                    unlocked.append(boxes[key])
-                    print(keys)
+            if boxes[key] not in unlocked:
+                unlocked.append(boxes[key])
 
     if len(unlocked) == len(boxes):
         return True
