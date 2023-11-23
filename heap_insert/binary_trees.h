e02117496a82afbb06e28057968887ebff1c22ea
NE0ID <neov0id.nova@gmail.com>
2023-11-23 04:22:25 -0800

    [Modify] binary_trees.h

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -22,25 +22,25 @@ typedef struct binary_tree_s
 typedef struct binary_tree_s heap_t;
 
 /**
- * struct stack_s - Stack
- *
- * @head: head of stack
- * @node: stored node of binary_tree
- * @next: ppointer to next node
+ * struct stack_tree_s - stack tree node
+ * @node: stocked binary_tree_t node
+ * @next: next stack node
  */
-typedef struct stack_s
+typedef struct stack_tree_s
 {
-	struct stack_s **head;
-	binary_tree_t *node;
-	struct stack_s *next;
+	struct stack_tree_s *head;
+	struct binary_tree_s *node;
+	struct stack_tree_s *next;
 } stack_t;
-
 /* Prototypes (example functions) */
 void binary_tree_print(const binary_tree_t *);
 
 /* Prototypes (my functions) */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int hasLeft(binary_tree_t *node);
+int hasRight(binary_tree_t *node);
 heap_t *heap_insert(heap_t **root, int value);
+void swap(binary_tree_t *node);
 
 #endif /* __BINARY_TREES_H__ */
